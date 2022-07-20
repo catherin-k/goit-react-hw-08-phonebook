6a79f0308ce88f52af43d3fe40923105e6ec5f94
Katarina Kyrylenko <katarina.kyrylenko@gmail.com>
2022-07-20 16:59:15 +0300

    initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,7 @@
-import user from './data/user';
-import statisticsData from './data/data.json';
-import friends from './data/friends.json';
-import transactions from './data/transactions.json';
-import { Profile } from './profile/Profile';
-import { Statistics } from './statistics/Statistics';
-import { FriendList } from './friends/FriendList';
-import { TransactionHistory } from './transactions/TransactionHistory';
-
 export const App = () => {
   return (
     <>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics title={'Upload stats'} stats={statisticsData} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
+      <h1>Hello!!!</h1>
     </>
   );
 };
--- src/components/statistics/randomColor.js
+++
@@ -1,3 +0,0 @@
-export const randomColor = () => {
-  return '#' + Math.random().toString(16).substring(9);
-};
