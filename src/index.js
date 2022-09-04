779dc2035d0c6f358ef359d323843bc9e0a56aea
Katarina Kyrylenko <katarina.kyrylenko@gmail.com>
2022-09-05 00:03:24 +0300

    delete one import

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { App } from 'components/App';
-import { store, persistor } from './redux/store';
-// import { PersistGate } from 'redux-persist/integration/react';
+import { store } from './redux/store';
+
 import './index.css';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
