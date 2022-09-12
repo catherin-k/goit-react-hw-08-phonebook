0b92bd01baeb452e24f43c38bbccc3415497ee9c
Katarina Kyrylenko <katarina.kyrylenko@gmail.com>
2022-09-12 09:32:35 +0300

    fixed the routing

--- src/components/authNav/AuthNav.jsx
+++ src/components/authNav/AuthNav.jsx
@@ -3,8 +3,8 @@ import { LinkStyled } from './AuthNav.styled';
 export const AuthNav = () => {
   return (
     <Box>
-      <LinkStyled to="/register">Registration</LinkStyled>
-      <LinkStyled to="/login">Log in</LinkStyled>
+      <LinkStyled to="register">Registration</LinkStyled>
+      <LinkStyled to="login">Log in</LinkStyled>
     </Box>
   );
 };
