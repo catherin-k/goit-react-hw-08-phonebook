a926452a18718c056f37baed97d266bf413a0006
Katarina Kyrylenko <katarina.kyrylenko@gmail.com>
2022-09-12 13:39:35 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import { lazy, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { Routes, Route } from 'react-router-dom';
-import { authOperations } from 'redux/auth';
+import { authOperations, authSelectors } from 'redux/auth';
 import { ShareLayout } from './shareLayout/ShareLayout';
 import PrivateRoute from './routes/PrivateRoute';
 import PublicRoute from './routes/PublicRoute';
@@ -14,7 +14,7 @@ const ContactsPage = lazy(() => import('pages/ContactsPage'));
 
 export const App = () => {
   const dispatch = useDispatch();
-  // const isRefreshingUser = useSelector(authSelectors.getIsRefreshingUser);
+  const isRefreshingUser = useSelector(authSelectors.getIsRefreshingUser);
 
   useEffect(() => {
     dispatch(authOperations.fetchCurrentUser());
@@ -22,43 +22,47 @@ export const App = () => {
 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<ShareLayout />}>
-          <Route
-            index
-            element={
-              <PublicRoute>
-                <HomePage />
-              </PublicRoute>
-            }
-          />
-          <Route
-            path="login"
-            element={
-              <PublicRoute restricted redirectTo="/contacts">
-                <LoginPage />
-              </PublicRoute>
-            }
-          />
-          <Route
-            path="register"
-            element={
-              <PublicRoute restricted redirectTo="/contacts">
-                <RegisterPage />
-              </PublicRoute>
-            }
-          />
+      {isRefreshingUser ? (
+        <h1>Loading...</h1>
+      ) : (
+        <Routes>
+          <Route path="/" element={<ShareLayout />}>
+            <Route
+              index
+              element={
+                <PublicRoute>
+                  <HomePage />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="login"
+              element={
+                <PublicRoute restricted redirectTo="/contacts">
+                  <LoginPage />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="register"
+              element={
+                <PublicRoute restricted redirectTo="/contacts">
+                  <RegisterPage />
+                </PublicRoute>
+              }
+            />
 
-          <Route
-            path="contacts"
-            element={
-              <PrivateRoute redirectTo="/login">
-                <ContactsPage />
-              </PrivateRoute>
-            }
-          />
-        </Route>
-      </Routes>
+            <Route
+              path="contacts"
+              element={
+                <PrivateRoute redirectTo="/login">
+                  <ContactsPage />
+                </PrivateRoute>
+              }
+            />
+          </Route>
+        </Routes>
+      )}
     </>
   );
 };
