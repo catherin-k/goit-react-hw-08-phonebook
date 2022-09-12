35902837e62d084f4154655c8d1c4db4891ed4b5
Katarina Kyrylenko <katarina.kyrylenko@gmail.com>
2022-09-12 14:53:38 +0300

    fix

--- src/components/routes/PrivateRoute.js
+++ src/components/routes/PrivateRoute.js
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import { authSelectors } from 'redux/auth';
 
-export default function PrivateRoute({ children, redirectTo = '/' }) {
+export default function PrivateRoute({ children, redirectTo }) {
   const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
   const isRefreshing = useSelector(authSelectors.getIsRefreshingUser);
 
